1423b142a97ca09cfbc09e0694de3c2162bc04ff
bo-nicolaisen <bni@techcollege.dk>
2025-11-26 13:03:29 +0100

    all done

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,13 +1,11 @@
 // write cool JS hwere!!
 
+// #region modal stuff
 // grab a handle to the modal button
-const modalTriggerButton=document.getElementById('modalTrigger')
-
-
-modalTriggerButton.addEventListener('click',toggleModal)
+const tilmeldButton=document.getElementById('modalTrigger')
+tilmeldButton.addEventListener('click',toggleModal)
 
 // close icon
-
 const closeIcon=document.getElementById('closeIcon')
 closeIcon.addEventListener('click',toggleModal)
 
@@ -16,6 +14,93 @@ closeIcon.addEventListener('click',toggleModal)
 
 // hides and show the window
 function toggleModal(){
+    clearForm()
       const modalElement=document.getElementById('modal')
   modalElement.classList.toggle('hidden')
 }
+
+
+
+
+// #endregion modal stuff
+
+
+
+
+
+// get submit button reference
+const submitButton=document.getElementById('formSubmit')
+
+// add eventlistner on click of submit button
+submitButton.addEventListener('click',(event)=>{ 
+    // prevents form default submit and page reload
+    event.preventDefault();
+
+    // calls the validateForm code as expression in the if statement
+    if(validateForm()){
+  console.log('form is valid');
+  // send data til server here
+    }else{
+    console.log('form is NOT valid');
+    }
+})      
+
+//---------------------------------------------------------------------
+
+// validates the form inputs with waterfall/cascade tests
+function validateForm(){
+
+    // get input values
+ const nameValue=document.getElementById('name').value
+    const emailValue=document.getElementById('email').value
+
+    // get user feedback element
+    const userFeedback=document.getElementById('message')
+
+
+
+    // name test
+    if (nameValue.length<2) {
+
+    console.log('Name is too short');
+userFeedback.innerText='Name is too short'
+        return false
+    }
+
+
+    // email test
+       const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,63}$/g;
+
+    // Test if the email matches the regex pattern
+    let emailValid = emailRegex.test(emailValue);
+
+    if (!emailValid) {
+   
+userFeedback.innerText+='Email is invalid'
+        return false
+    }
+
+
+
+/// all tests passed
+  userFeedback.innerText='thanks for submitting the form!'
+    return true
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+function clearForm() {
+    document.getElementById('name').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('message').innerText = '';
+}   
