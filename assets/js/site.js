e4204ab91d19851d71c0225a0ec6827c68a43054
bo-nicolaisen <bni@techcollege.dk>
2025-11-26 10:41:51 +0100

    modal trigger

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,1 +1,21 @@
 // write cool JS hwere!!
+
+// grab a handle to the modal button
+const modalTriggerButton=document.getElementById('modalTrigger')
+
+
+modalTriggerButton.addEventListener('click',toggleModal)
+
+// close icon
+
+const closeIcon=document.getElementById('closeIcon')
+closeIcon.addEventListener('click',toggleModal)
+
+
+
+
+// hides and show the window
+function toggleModal(){
+      const modalElement=document.getElementById('modal')
+  modalElement.classList.toggle('hidden')
+}
